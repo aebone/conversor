88f9b5802e4c528bd766f8102669ee3b47e8f04f
Aline Ebone <iniebone@gmail.com>
2020-10-06 18:54:29 -0300

    Updates

---
+++ precache-manifest.22cae1e8260a7410d576e2a20e0131e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b06676ca81fcd17ed760533a115152",
+    "url": "/conversor/index.html"
+  },
+  {
+    "revision": "31113312a139340c7920",
+    "url": "/conversor/static/js/2.fc0364c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/conversor/static/js/2.fc0364c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4ce27fe2e09bca004d",
+    "url": "/conversor/static/js/main.5021f424.chunk.js"
+  },
+  {
+    "revision": "d976c98bed6d4bd308cb",
+    "url": "/conversor/static/js/runtime-main.c3e9902b.js"
+  }
+]);
