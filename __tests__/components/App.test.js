03cf579e548c050a6439cfd61f144b1c5561d9bb
Aline Ebone <iniebone@gmail.com>
2020-10-10 09:30:34 -0300

    App component tests

--- __tests__/components/App.test.js
+++ __tests__/components/App.test.js
@@ -1,8 +1,23 @@
 import React from "react";
 import App from "../../src/components/App";
-import { mount } from "../../src/enzyme";
+import { mount, shallow } from "../../src/enzyme";
 
-test("App snapshot", () => {
-  const component = mount(<App />);
-  expect(component).toMatchSnapshot();
+describe("App component", () => {
+  it("should mount and match snapshot", () => {
+    const component = mount(<App />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it("should start with light theme", function () {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state("theme")).toBe("light");
+  });
+
+  it("should toggle between dark and light theme when changeTheme() is called", function () {
+    const wrapper = shallow(<App />);
+    wrapper.instance().changeTheme();
+    expect(wrapper.state("theme")).toBe("dark");
+    wrapper.instance().changeTheme();
+    expect(wrapper.state("theme")).toBe("light");
+  });
 });
