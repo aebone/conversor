d1a8dca52cb53327256c259cedb540b0da744803
Aline Ebone <iniebone@gmail.com>
2020-10-10 00:10:59 -0300

    Add enzyme

--- __tests__/components/App.test.js
+++ __tests__/components/App.test.js
@@ -1,9 +1,8 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import App from "../../src/components/App";
+import { mount } from "../../src/enzyme";
 
 test("App snapshot", () => {
-  const component = renderer.create(<App />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = mount(<App />);
+  expect(component).toMatchSnapshot();
 });
