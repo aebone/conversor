d243a892203f61a5bbda0d44c3f8e4be37583e73
Aline Ebone <iniebone@gmail.com>
2020-10-10 21:59:36 -0300

    Lightbulb width test

---
+++ __tests__/components/icons/LightBulb.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { shallow } from "../../../src/enzyme";
+import LightBulb from "../../../src/components/icons/LightBulb";
+
+describe("LightBulb component", () => {
+  it("should return default width if none is passed", () => {
+    const defaultWidth = 50;
+    const wrapper = shallow(<LightBulb />);
+    expect(wrapper.props().width).toBe(defaultWidth);
+  });
+
+  it("should return custom passed width", () => {
+    const customWidth = 100;
+    const wrapper = shallow(<LightBulb width={customWidth} />);
+    expect(wrapper.props().width).toBe(customWidth);
+  });
+});
